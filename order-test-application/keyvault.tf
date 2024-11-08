b3f33e47eff9705683a917e49e94bf8437cba787
ul-victorsantos <108574@global.ul.com>
2024-11-08 09:13:11 -0300

    keyvault

---
+++ order-test-application/keyvault.tf
@@ -0,0 +1,7 @@
+resource "azurerm_key_vault" "main" {
+  name                = var.key_vault_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku_name            = "standard"
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+}
